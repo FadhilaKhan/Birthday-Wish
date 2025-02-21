68a9f4dc19f42eef50a1e188055970865a5f2eaa
Fadhila <fadhilak500@gmail.com>
2025-02-21 14:14:52 +0530

    updated

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 let currentPage = 0;
 const pages = document.querySelectorAll(".page");
-const nextButton = document.getElementById("next");
-const prevButton = document.getElementById("prev");
 
-// Update the button states (enable/disable) based on current page
-function updateButtons() {
-    // Disable the next button if it's the last page
-    nextButton.disabled = currentPage === pages.length - 1;
 
-    // Disable the previous button if it's the first page
-    prevButton.disabled = currentPage === 0;
-}
-
-// Show or hide the pages based on the page index
 function showPage(pageIndex) {
     // Hide all pages
     pages.forEach(page => {
@@ -21,13 +10,9 @@ function showPage(pageIndex) {
 
     // Show the current page
     pages[pageIndex].classList.add("visible");
-
-    // Update the button states
-    updateButtons();
 }
 
-// Next button click functionality
-nextButton.addEventListener("click", () => {
+document.getElementById("next").addEventListener("click", () => {
     if (currentPage < pages.length - 1) {
         // Flip the current page
         pages[currentPage].classList.add("flipped");
@@ -40,8 +25,7 @@ nextButton.addEventListener("click", () => {
     }
 });
 
-// Previous button click functionality
-prevButton.addEventListener("click", () => {
+document.getElementById("prev").addEventListener("click", () => {
     if (currentPage > 0) {
         // Unflip the current page
         pages[currentPage].classList.remove("flipped");
